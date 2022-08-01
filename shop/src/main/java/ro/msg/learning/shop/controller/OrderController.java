419a6265ba70b6ee32ae83e5abbb112f07acd2f8
Rares Birau <Rares.Birau@msg.group>
2022-08-01 16:25:56 +0300

    Chapter 4 - strategy2

--- shop/src/main/java/ro/msg/learning/shop/controller/OrderController.java
+++ shop/src/main/java/ro/msg/learning/shop/controller/OrderController.java
@@ -7,31 +7,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping(value = "/order")
 public class OrderController {
-//    private final OrderService orderService;
-//    private final ProductService productService;
-//    private final CustomerService customerService;
-//
-//    @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
-//    @Transactional
-//    public ResponseEntity<?> creteOrder(@RequestBody ProductOrderDTO orderDTOView) {
-//        try {
-//            Map<Product, Integer> productQuantity = orderDTOView.getProductsList().stream()
-//                    .collect(Collectors.toMap(x -> productService.readById(x.getProductId()), ProductOrderDto::getQuantity));
-//            ProductOrder order = ProductOrder.builder().city(orderDTOView.getCity()).country(orderDTOView.getCountry()).createAt(orderDTOView.getTimestamp()).streetAddress(orderDTOView.getStreetAddress()).county(orderDTOView.getCounty()).customer(customerService.findById(1)).build();
-//
-//            List<OrdersDto> ordersDtos = orderService.createOrder(productQuantity, order).stream()
-//                    .map(o -> OrdersDto.builder().id(o.getId()).city(o.getCity()).country(o.getCountry()).county(o.getCounty()).streetAddress(o.getStreetAddress()).createAt(o.getCreateAt()).customerId(o.getCustomer().getId()).customerName(o.getCustomer().getFirstName() + o.getCustomer().getLastName()).locationId(o.getLocation().getId()).locationName(o.getLocation().getName()).build())
-//                    .collect(Collectors.toList());
-//
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.add("Created", MediaType.APPLICATION_JSON_VALUE);
-//            return ResponseEntity.accepted().headers(headers).body(ordersDtos);
-//        } catch (StrategyException | NoObjectFoundException e) {
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.add("Not created", MediaType.APPLICATION_JSON_VALUE);
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(headers).body(e.getMessage());
-//        }
 
-//    }
 
 }
--- shop/src/main/java/ro/msg/learning/shop/controller/ProductController.java
+++ shop/src/main/java/ro/msg/learning/shop/controller/ProductController.java
@@ -1,13 +1,9 @@
 package ro.msg.learning.shop.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ro.msg.learning.shop.exception.ProductNotFoundException;
 import ro.msg.learning.shop.model.Product;
 import ro.msg.learning.shop.repository.IProductRepository;
-import ro.msg.learning.shop.service.ProductService;
+
 
 import java.util.List;
 
@@ -24,14 +20,13 @@ public class ProductController {
     List<Product> all() {
         return product.findAll();
     }
-    // end::get-aggregate-root[]
+
 
     @PostMapping("/products")
     Product NewProduct(@RequestBody Product newProduct) {
         return product.save(newProduct);
     }
 
-    // Single item
 
     @GetMapping("/products/{id}")
     Product one(@PathVariable Integer id) {
